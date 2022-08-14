e90eb857e2c06f7cd1b51e013d03146f021566ea
Tomer Hananel <tomnanaustes@gmail.com>
2022-08-14 17:22:57 +0300

    Added ESP8266 code

---
+++ FinalProject_ESP8266/iot_configs.h
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+// change this to the actual WiFi network name
+#define IOT_CONFIG_WIFI_SSID "WIFI-SSID"
+// change this to the actual WiFi network password
+#define IOT_CONFIG_WIFI_PASSWORD "WIFI-PASSWORD"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "IoTHubProject.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "pir1"
+#define IOT_CONFIG_DEVICE_KEY "QkDv5N/cO7CU15CEFaErRLZGsZpmK/p2TNik6/pthM4="
